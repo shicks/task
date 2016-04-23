2d98d3aefda483cb83da774ecbddf59c18289ad5
Stephen Hicks <sdh33@cornell.edu>
2016-04-23 15:40:12 -0700

    fix

--- ed.pl
+++ ed.pl
@@ -1,14 +1,22 @@
 #!/usr/bin/perl
 
-use IO::Select;
+use File::Temp;
+#use IO::Select;
 
 my $file = $ARGV[0];
-my $/ = undef;
-my $contents = <>;
-open OUT, ">$ARGV[0]";
+my $temp = File::Temp->new();
+my $tempname = $temp->filename;
+close $temp;
+system "cp $file $tempname";
+
+system "cat $ENV{'READ'} > $tempname &"
+system "cat $ENV{'
+
+open OUT, ">$file";
+
 
 open my $inh, "<$ENV{'READ'}";
-open my $outh, ">$ENV{'WRITE'}";
+open my $outh, ">>$ENV{'WRITE'}";
 
 my $rd = IO::Select->new;
 my $wr = IO::Select->new;
@@ -17,27 +25,35 @@ $rd->add($inh);
 $wr->add($outh);
 
 while ($rd->handles or $wr->handles) {
-  my @ready = ($rd->can_write(0.001), $wr->can_read(0));
-  for my $fh (@ready) { # loop through them
-    if ($fh == $inh) {
-      my $chunk;
-      my $len = sysread $outh, $chunk, 4096;
-      if ($len) {
-        print OUT $chunk;
-      } else {
-        $rd->remove $inh;
-        close $inh;
-      }
-    } elsif ($fh == $outh) {
+  my @ready = $wr->can_write(0);
+  print STDERR "READY WRITE: @ready\n";
+  my @ready2 = $rd->can_read(0);
+  print STDERR "READY READ: @ready2\n";
+  for my $fh (@ready, @ready2) { # loop through them
+    print STDERR "READY: @ready\n";
+    if ($fh == $outh) {
+      print STDERR "READY: OUT $fh\n";
       if (length $contents) {
         my $written = syswrite $outh, $contents;
         $contents = substr($contents, $written);
       } else {
-        $wr->remove $outh;
+        $wr->remove($outh);
         close $outh;
+      }
+    } elsif ($fh == $inh) {
+      print STDERR "READY: IN $fh\n";
+      my $chunk;
+      my $len = sysread $inh, $chunk, 4096;
+      if ($len) {
+        #print STDERR "CHUNK: *OUT $chunk\n";
+        print OUT $chunk;
+      } else {
+        $rd->remove($inh);
+        close $inh;
       }
     }
   }
 }
 
 close OUT;
+#system "cat $ARGV[0]";
